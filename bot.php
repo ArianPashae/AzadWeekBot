8307cf3fa97d54fd9191ca9290aacbf19f564b27
Arian Pashae <info@arianpashae.com>
2024-11-10 00:02:45 +0330

    Update bot.php
    optimize function saveUserInfo

--- bot.php
+++ bot.php
@@ -13,11 +13,8 @@ $user_file = 'users.json'; // New file to store user information
 // Function to store and retrieve user information
 function saveUserInfo($chat_id, $username) {
     global $user_file;
-    if (!file_exists($user_file)) {
-        file_put_contents($user_file, json_encode([]));
-    }
-    $users = json_decode(file_get_contents($user_file), true);
-    $users[$chat_id] = ['username' => $username, 'joined_at' => time()]; // Save username and membership time
+    $users = file_exists($user_file) ? json_decode(file_get_contents($user_file), true) : [];
+    $users[$chat_id] = ['username' => $username, 'joined_at' => time()]; // ذخیره نام کاربر و زمان عضویت
     file_put_contents($user_file, json_encode($users));
 }
 ?>
